fba347fa3db2b0a30e1f5936ce6cb9d2b987fcdc
Kensuke Saito <ken.saito.0813@gmail.com>
2020-07-15 15:48:07 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 mkdir tri-echo/build
 cd tri-echo/build; cmake ..; make; sudo cp triangular_echo /usr/local/bin
-echo 'alias tri-echo=". /usr/local/bin"' >> $HOME/.bashrc
+echo 'alias tri-echo=". /usr/local/bin/triangular_echo"' >> $HOME/.bashrc
